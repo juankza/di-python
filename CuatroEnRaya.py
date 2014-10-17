c98316caa11b18ceaae2e4c1f3f903eb20e17b42
juankza <cazalillacostaj@gmail.com>
2014-10-17 13:59:58 +0200

    Cambios

--- CuatroEnRaya.py
+++ CuatroEnRaya.py
@@ -11,18 +11,20 @@ def drawBoard(board):
     os.system('clear')
     # board[6x7]
     # "board" is a matrix with 6 rows and 7 columns of strings
-    print(' ' +board[5][0]+ ' | ' +board[5][1]+ ' | ' +board[5][2]+ ' | ' +board[5][3]+ ' | ' +board[5][4]+ ' | ' +board[5][5]+ ' | ' +board[5][6]+ ' ')
-    print('---------------------------')
-    print(' ' +board[4][0]+ ' | ' +board[4][1]+ ' | ' +board[4][2]+ ' | ' +board[4][3]+ ' | ' +board[4][4]+ ' | ' +board[4][5]+ ' | ' +board[4][6]+ ' ')
-    print('---------------------------')
-    print(' ' +board[3][0]+ ' | ' +board[3][1]+ ' | ' +board[3][2]+ ' | ' +board[3][3]+ ' | ' +board[3][4]+ ' | ' +board[3][5]+ ' | ' +board[3][6]+ ' ')
-    print('---------------------------')
-    print(' ' +board[2][0]+ ' | ' +board[2][1]+ ' | ' +board[2][2]+ ' | ' +board[2][3]+ ' | ' +board[2][4]+ ' | ' +board[2][5]+ ' | ' +board[2][6]+ ' ')
-    print('---------------------------')
-    print(' ' +board[1][0]+ ' | ' +board[1][1]+ ' | ' +board[1][2]+ ' | ' +board[1][3]+ ' | ' +board[1][4]+ ' | ' +board[1][5]+ ' | ' +board[1][6]+ ' ')
-    print('---------------------------')
-    print(' ' +board[0][0]+ ' | ' +board[0][1]+ ' | ' +board[0][2]+ ' | ' +board[0][3]+ ' | ' +board[0][4]+ ' | ' +board[0][5]+ ' | ' +board[0][6]+ ' ')
-
+    print('| ' +board[5][0]+ ' | ' +board[5][1]+ ' | ' +board[5][2]+ ' | ' +board[5][3]+ ' | ' +board[5][4]+ ' | ' +board[5][5]+ ' | ' +board[5][6]+ ' |')
+    print('-----------------------------')
+    print('| ' +board[4][0]+ ' | ' +board[4][1]+ ' | ' +board[4][2]+ ' | ' +board[4][3]+ ' | ' +board[4][4]+ ' | ' +board[4][5]+ ' | ' +board[4][6]+ ' |')
+    print('-----------------------------')
+    print('| ' +board[3][0]+ ' | ' +board[3][1]+ ' | ' +board[3][2]+ ' | ' +board[3][3]+ ' | ' +board[3][4]+ ' | ' +board[3][5]+ ' | ' +board[3][6]+ ' |')
+    print('-----------------------------')
+    print('| ' +board[2][0]+ ' | ' +board[2][1]+ ' | ' +board[2][2]+ ' | ' +board[2][3]+ ' | ' +board[2][4]+ ' | ' +board[2][5]+ ' | ' +board[2][6]+ ' |')
+    print('-----------------------------')
+    print('| ' +board[1][0]+ ' | ' +board[1][1]+ ' | ' +board[1][2]+ ' | ' +board[1][3]+ ' | ' +board[1][4]+ ' | ' +board[1][5]+ ' | ' +board[1][6]+ ' |')
+    print('-----------------------------')
+    print('| ' +board[0][0]+ ' | ' +board[0][1]+ ' | ' +board[0][2]+ ' | ' +board[0][3]+ ' | ' +board[0][4]+ ' | ' +board[0][5]+ ' | ' +board[0][6]+ ' |')
+    print('-----------------------------')
+    print('| 1 | 2 | 3 | 4 | 5 | 6 | 7 |')
+    print('')
 def inputPlayerLetter():
     # Let's the player type which letter they want to be.
     # Returns a list with the player's letter as the first item, and the
@@ -62,7 +64,14 @@ def playAgain():
         return False
 
 def makeMove(board, letter, move):
-    board[int(move[0]) -1][int(move[1]) -1] = letter
+    x = 0
+    for i in range(len(board)):
+        moveList = [x, move] 
+        if isSpaceFree(board, moveList):
+            board[int(moveList[0])][int(moveList[1])] = letter
+            break
+        else:
+            x = x +1
 ######################### Arreglado isWinner(board, letter)        ####SEMI-Arreglado
 def isWinner(board, letter):
     # Given a board and a player's letter, this function returns True if that
@@ -74,7 +83,7 @@ def isWinner(board, letter):
     for i in board:
         y = 0
         for j in board[x]:
-            if y < 4 or win == False:
+            if y < 4 and win == False:
                 if (board[x][y +0] == letter and board[x][y +1] == letter and board[x][y +2] == letter and board[x][y +3] == letter):
                     win = True
                 else:
@@ -87,7 +96,7 @@ def isWinner(board, letter):
     for i in board[y]:
         x = 0
         for j in board:
-            if x < 3 or win == False:
+            if x < 3 and win == False:
                 if (board[x +0][y] == letter and board[x +1][y] == letter and board[x +2][y] == letter and board[x +3][y] == letter):
                     win = True
                 else:
@@ -120,11 +129,17 @@ def isSpaceFree(board, move):
 ######################### Arreglado getPlayerMove(board)
 def getPlayerMove(board):
     # Let the player type in his move.
-    move = [' ', ' ']
-    while move[0] not in '0 1 2 3 4 5'.split() or move[1] not in '0 1 2 3 4 5 6'.split() or not isSpaceFree(board, move):
-        move[0] = int(raw_input('What is your next ROW move? (1-6)\t')) -1
-        move[1] = int(raw_input('What is your next COLUMN move? (1-7)\t')) -1
-    return move
+    x = 0
+    move = raw_input('What is your next move? (1-7)\t')
+    for i in range(len(board)):
+        moveList = [x, (int(move) -1)]
+        if move in '1 2 3 4 5 6 7'.split():
+            if isSpaceFree(board, moveList) == True:
+                return (int(move) -1)
+            else:
+                x = x +1
+        else:
+            move = raw_input('What is your next move? (1-7)\t')
 ############################################################################################### Arreglado hasta aquí
 def chooseRandomMoveFromList(board, movesList):
     # Returns a valid move from the passed list on the passed board.
@@ -138,7 +153,7 @@ def chooseRandomMoveFromList(board, movesList):
         return random.choice(possibleMoves)
     else:
         return None
-
+#########################SEMI-Arreglado
 def getComputerMove(board, computerLetter):
     # Given a board and the computer's letter, determine where to move and
     # return that move.
@@ -146,25 +161,34 @@ def getComputerMove(board, computerLetter):
         playerLetter = 'O'
     else:
         playerLetter = 'X'
-
     # Here is our algorithm for our Tic Tac Toe AI:
     # First, check if we can win in the next move
-    for i in range(1, 43):
-        copy = getBoardCopy(board)
-        if isSpaceFree(copy, i):
-            makeMove(copy, computerLetter, i)
-            if isWinner(copy, computerLetter):
-                return i
-
+    x = 0
+    for i in range(len(board)):
+        y = 0
+        for j in range(len(board[x])):
+            copy = getBoardCopy(board)
+            move = [x, y]
+            if isSpaceFree(copy, move):
+                makeMove(copy, computerLetter, move)
+                if isWinner(copy, playerLetter):
+                    return move
+            y = y +1
+        x = x +1
     # Check if the player could win on his next move, and block them.
-    for i in range(1, 43):
-        copy = getBoardCopy(board)
-        if isSpaceFree(copy, i):
-            makeMove(copy, playerLetter, i)
-            if isWinner(copy, playerLetter):
-                return i
-
-    # Try to take one of the corners, if they are free.
+    x = 0
+    for i in range(len(board)):
+        y = 0
+        for j in range(len(board[x])):
+            copy = getBoardCopy(board)
+            move = [x, y]
+            if isSpaceFree(copy, move):
+                makeMove(copy, playerLetter, move)
+                if isWinner(copy, playerLetter):
+                    return move
+            y = y +1
+        x = x +1
+    # Try to take one of the corners, if they are free.  ##########
     move = chooseRandomMoveFromList(board, [1, 3, 7, 9])
     if move != None:
         return move
