670b7fdb927cc6a75fc2b27fd692a3c6a46174f5
juankza <cazalillacostaj@gmail.com>
2014-10-18 12:53:45 +0200

    Cambios

--- CuatroEnRaya.py
+++ CuatroEnRaya.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys
 import os
 import random
+import sys
 
 def drawBoard(board):
-    # This function prints out the board that it was passed.
-    # "board" is a matrix with 6 rows and 7 columns of strings (6x7).
+    # This function prints out the board that it was passed
+    # That "board" is a matrix with 6 rows and 7 columns of strings (6x7)
     os.system('clear')
     print('| ' +board[5][0]+ ' | ' +board[5][1]+ ' | ' +board[5][2]+ ' | ' +board[5][3]+ ' | ' +board[5][4]+ ' | ' +board[5][5]+ ' | ' +board[5][6]+ ' |')
     print('-----------------------------')
@@ -23,33 +23,28 @@ def drawBoard(board):
     print('-----------------------------')
     print('| 1 | 2 | 3 | 4 | 5 | 6 | 7 |')
     print('')
+
 def inputPlayerLetter():
-    # Let's the player type which letter they want to be.
-    # Returns a list with the player's letter as the first item, and the
-    # computer's letter as the second.
+    # Let's the player type which letter they want to be
+    # Returns a list with the player's letter as the first item, and the computer's letter as the second
     letter = ''
     while not (letter == 'X' or letter == 'x' or letter == 'O' or letter == 'o'):
         letter = raw_input('Do you want to be X or O?\t')
-
-    # the first element in the tuple is the player's letter, the second is
-    # the computer's letter.
     if letter == 'X' or letter == 'x':
         return ['X', 'O']
     else:
         return ['O', 'X']
 
 def whoGoesFirst():
-    # Randomly choose the player who goes first.
+    # Randomly choose the player who goes first
     if random.randint(0, 1) == 0:
         return 'computer'
     else:
         return 'player'
 
 def playAgain():
-    # This function returns True if the player wants to play again, otherwise
-    # it returns False.
+    # This function returns True if the player wants to play again, otherwise it returns False
     again = raw_input('Do you want to play again? (y or n)\t')
-
     if again == 'Y' or again == 'y' or again == 'Yes' or again == 'yes':
         return True
     elif again == 'N' or again == 'n' or again == 'No' or again == 'no' or again == '':
@@ -68,11 +63,9 @@ def makeMove(board, letter, move):
             x = x +1
 
 def isWinner(board, letter):
-    # Given a board and a player's letter, this function returns True if that
-    # player has won. We use bo instead of board and le instead of letter so
-    # we don't have to type as much.
+    # Given a board and a player's letter, this function returns True checking if that player has won
     win = False
-    #FOR HorizontalWin (ROW)
+    # FOR HorizontalWin (ROW)
     x = 0
     for i in board:
         y = 0
@@ -85,7 +78,7 @@ def isWinner(board, letter):
             else:
                 j = len(board[x])
         x = x +1
-    #FOR VerticalWin (COLUMN)
+    # FOR VerticalWin (COLUMN)
     y = 0
     for i in board[y]:
         x = 0
@@ -98,13 +91,42 @@ def isWinner(board, letter):
             else:
                 j = len(board)
         y = y +1
-########################################TODO DiagonalWin
+    # IF DiagonalWin (LEFT-TO-RIGHT)
+    if ((board[2][0] == letter and board[3][1] == letter and board[4][2] == letter and board[5][3] == letter) or
+    (board[1][0] == letter and board[2][1] == letter and board[3][2] == letter and board[4][3] == letter) or
+    (board[2][1] == letter and board[3][2] == letter and board[4][3] == letter and board[5][4] == letter) or
+    (board[0][0] == letter and board[1][1] == letter and board[2][2] == letter and board[3][3] == letter) or
+    (board[1][1] == letter and board[2][2] == letter and board[3][3] == letter and board[4][4] == letter) or
+    (board[2][2] == letter and board[3][3] == letter and board[4][4] == letter and board[5][5] == letter) or
+    (board[0][1] == letter and board[1][2] == letter and board[2][3] == letter and board[3][4] == letter) or
+    (board[1][2] == letter and board[2][3] == letter and board[3][4] == letter and board[4][5] == letter) or
+    (board[2][3] == letter and board[3][4] == letter and board[4][5] == letter and board[5][6] == letter) or
+    (board[0][2] == letter and board[1][3] == letter and board[2][4] == letter and board[3][5] == letter) or
+    (board[1][3] == letter and board[2][4] == letter and board[3][5] == letter and board[4][6] == letter) or
+    (board[0][3] == letter and board[1][4] == letter and board[2][5] == letter and board[3][6] == letter)):
+        win = True
+    else:
+        win = False
+    # IF DiagonalWin (RIGHT-TO-LEFT)
+    if ((board[2][6] == letter and board[3][5] == letter and board[4][4] == letter and board[5][3] == letter) or
+    (board[1][6] == letter and board[2][5] == letter and board[3][4] == letter and board[4][3] == letter) or
+    (board[2][5] == letter and board[3][4] == letter and board[4][3] == letter and board[5][2] == letter) or
+    (board[0][6] == letter and board[1][5] == letter and board[2][4] == letter and board[3][3] == letter) or
+    (board[1][5] == letter and board[2][4] == letter and board[3][3] == letter and board[4][2] == letter) or
+    (board[2][4] == letter and board[3][3] == letter and board[4][2] == letter and board[5][1] == letter) or
+    (board[0][5] == letter and board[1][4] == letter and board[2][3] == letter and board[3][2] == letter) or
+    (board[1][4] == letter and board[2][3] == letter and board[3][2] == letter and board[4][1] == letter) or
+    (board[2][3] == letter and board[3][2] == letter and board[4][1] == letter and board[5][0] == letter) or
+    (board[0][4] == letter and board[1][3] == letter and board[2][2] == letter and board[3][1] == letter) or
+    (board[1][3] == letter and board[2][2] == letter and board[3][1] == letter and board[4][0] == letter) or
+    (board[0][3] == letter and board[1][2] == letter and board[2][1] == letter and board[3][0] == letter)):
+        win = True
+    else:
+        win = False
     return win
-#    (board[7] == letter and board[5] == letter and board[3] == letter) or # diagonal
-#    (board[9] == letter and board[5] == letter and board[1] == letter) # diagonal
 
 def getBoardCopy(board):
-    # Make a duplicate of the board list and return it the duplicate.
+    # Make a duplicate of the board list and return it the duplicate
     copyBoard = []
     for i in range(6):
         copyBoard.append([' '] * 7)
@@ -118,11 +140,11 @@ def getBoardCopy(board):
     return copyBoard
 
 def isSpaceFree(board, moveList):
-    # Return true if the passed move is free on the passed board.
+    # Return true if the passed move is free on the passed board
     return board[int(moveList[0])][int(moveList[1])] == ' '
 
 def getPlayerMove(board):
-    # Let the player type in his move.
+    # Let the player type in his move
     x = 0
     move = raw_input('What is your next move? (1-7)\t')
     for i in range(len(board)):
@@ -136,8 +158,8 @@ def getPlayerMove(board):
             move = raw_input('What is your next move? (1-7)\t')
 
 def getRandomMove(board):
-    # Returns a random valid move on the passed board.
-    # Returns None if there is no valid move. ####random.randint(0, 1)
+    # Returns a random valid move on the passed board
+    # Returns None if there is no valid move
     canMove = []
     x = 0
     for i in range(len(board)):
@@ -149,20 +171,18 @@ def getRandomMove(board):
             else:
                 y = y +1
         x = x +1
-
     if len(canMove) != 0:
         return random.choice(canMove)
     else:
         return None
 
 def getComputerMove(board, computerLetter):
-    # Given a board and the computer's letter, determine where to move and
-    # return that move.
+    # Given a board and the computer's letter, determine where to move and return that move
     if computerLetter == 'X':
         playerLetter = 'O'
     else:
         playerLetter = 'X'
-    # Here is the algorithm for ComputerAI:
+    ##### ComputerAI #####
     # First, check if we can win in the next move
     x = 0
     for i in range(len(board)):
@@ -176,7 +196,7 @@ def getComputerMove(board, computerLetter):
                     return y
             y = y +1
         x = x +1
-    # Check if the player could win on his next move, and block them.
+    # Check if the player could win on his next move to block them
     x = 0
     for i in range(len(board)):
         y = 0
@@ -189,14 +209,13 @@ def getComputerMove(board, computerLetter):
                     return y
             y = y +1
         x = x +1
-    # Try to take one of the corners, if they are free.
+    # Computer does a random move
     move = getRandomMove(board)
     if move != None:
         return move
 
 def isBoardFull(board):
-    # Return True if every space on the board has been taken. Otherwise
-    # return False.
+    # Return True if every space on the board has been taken, otherwise return False
     x = 0
     for i in range(len(board)):
         y = 0
@@ -208,8 +227,7 @@ def isBoardFull(board):
         x = x +1
     return True
 
-################################################### MAIN ###################################################
-
+#################### MAIN FUNCTION ####################
 while True:
     # Reset the board
     theBoard = []
@@ -220,12 +238,11 @@ while True:
     playerLetter, computerLetter = inputPlayerLetter()
     turn = whoGoesFirst()
     print('The ' + turn + ' will go first.')
-    wait = raw_input('\nPress Enter to continue...');
+    waitUser = raw_input('\nPress Enter to continue...');
     gameIsPlaying = True
-
     while gameIsPlaying:
         if turn == 'player':
-            # Player's turn.
+            # Player's turn
             drawBoard(theBoard)
             move = getPlayerMove(theBoard)
             makeMove(theBoard, playerLetter, move)
@@ -241,7 +258,7 @@ while True:
                 else:
                     turn = 'computer'
         else:
-            # Computer's turn.
+            # Computer's turn
             move = getComputerMove(theBoard, computerLetter)
             makeMove(theBoard, computerLetter, move)
 
@@ -256,6 +273,8 @@ while True:
                     break
                 else:
                     turn = 'player'
+
     if not playAgain():
         break
+
 os.system('clear')
