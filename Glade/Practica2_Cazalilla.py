1aef556117ab11d727d4209d0c252e7f5be3ae83
juankza <cazalillacostaj@gmail.com>
2014-11-06 12:32:31 +0100

    Cambios

--- Glade/Practica2_Cazalilla.py
+++ Glade/Practica2_Cazalilla.py
@@ -1,56 +1,56 @@
 #! /usr/bin/env python
 # -*- coding: UTF-8 -*-
 
+import sys
+import sqlite3
+bd = "tEjercicio"
 from gi.repository import Gtk
-glade = Gtk.Builder()
-glade.add_from_file("Practica2_Cazalilla.glade")
-window = glade.get_object("mainWindow")
-window.show_all()
-Gtk.main()
 
-##### MAIN CLASS
-class MainWin:
+def userEntryChanged(self):
+    print "on_userEntry_changed"
 
-	def __init__(self):
-		self.widgets = gtk.glade.XML("Practica2_Cazalilla.glade")
+def pwdEntryChanged(self):
+    print "on_pwdEntry_changed"
 
-        signals = { "on_userEntry_changed": self.on_userEntry_changed,
-        			"on_pwdEntry_changed": self.on_pwdEntry_changed,
-        			"on_emailEntry_changed": self.on_emailEntry_changed,
-        			"on_nameEntry_changed": self.on_nameEntry_changed,
-        			"on_surnameEntry_changed": self.on_surnameEntry_changed,
-        			"on_dirEntry_changed": self.on_dirEntry_changed,
-        			"on_cleanBtn_clicked": self.on_cleanBtn_clicked,
-        			"on_cancelBtn_clicked": self.on_cancelBtn_clicked,
-        			"on_okBtn_clicked": self.on_okBtn_clicked,
-        			"gtk_main_quit": gtk.main_quit }
+def emailEntryChanged():
+    print "on_emailEntry_changed"
 
-        self.widgets.signal_autoconnect(signals)
+def nameEntryChanged():
+    print "on_emailEntry_changed"
 
-    def on_cleanBtn_clicked(self, widget):
-    	# Entry Cleaner Function
-    	self.userEntry = self.widgets.get_object("userEntry")
-    	self.pwdEntry = self.widgets.get_object("pwdEntry")
-    	self.emailEntry = self.widgets.get_object("emailEntry")
-    	self.nameEntry = self.widgets.get_object("nameEntry")
-    	self.surnameEntry = self.widgets.get_object("surnameEntry")
-    	self.dirEntry = self.widgets.get_object("dirEntry")
+def surnameEntryChanged():
+    print "on_emailEntry_changed"
 
-    	self.userEntry.set_text("")
-   		self.pwdEntry.set_text("")
-   		self.emailEntry.set_text("")
-   		self.nameEntry.set_text("")
-   		self.surnameEntry.set_text("")
-   		self.dirEntry.set_text("")
+def dirEntryChanged():
+    print "on_emailEntry_changed"
 
-   		self.on_userEntry_changed(self)
-   		self.on_pwdEntry_changed(self)
-   		self.on_emailEntry_changed(self)
-   		self.on_nameEntry_changed(self)
-   		self.on_surnameEntry_changed(self)
-   		self.on_dirEntry_changed(self)
+def cleanBtnClicked(self):
+    print "cleanBtnClicked"
+    # Entry Cleaner Function
+    glade.get_object("userEntry").set_text("")
+    glade.get_object("pwdEntry").set_text("")
+    glade.get_object("emailEntry").set_text("")
+    glade.get_object("nameEntry").set_text("")
+    glade.get_object("surnameEntry").set_text("")
+    glade.get_object("dirEntry").set_text("")
+
+def cancelBtnClicked():
+    print "cancelBtnClicked"
+
+def okBtnClicked():
+    print "okBtnClicked"
+
+def gtk_main_quit():
+    Gtk.main_quit()
+
 
 ######################### MAIN #########################
 if __name__ == "__main__":
-	MainWin()
-	gtk.main()
+    glade = Gtk.Builder()
+    glade.add_from_file("Practica2_Cazalilla.glade")
+    signals = { "on_userEntry_changed": userEntryChanged,"on_pwdEntry_changed": pwdEntryChanged,"on_emailEntry_changed": emailEntryChanged,"on_nameEntry_changed": nameEntryChanged,"on_surnameEntry_changed": surnameEntryChanged,"on_dirEntry_changed": dirEntryChanged,"on_cleanBtn_clicked": cleanBtnClicked,"on_cancelBtn_clicked": cancelBtnClicked,"on_okBtn_clicked": okBtnClicked,"gtk_main_quit": Gtk.main_quit }
+    glade.connect_signals(signals)
+    window = glade.get_object("mainWindow")
+    window.show_all()
+    Gtk.main()
+    
