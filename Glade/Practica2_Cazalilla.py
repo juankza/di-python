2de3e64cf6f609264a3b09a12310ce78440e8e99
juankza <cazalillacostaj@gmail.com>
2014-11-11 12:53:56 +0100

    Cambios

--- Glade/Practica2_Cazalilla.py
+++ Glade/Practica2_Cazalilla.py
@@ -6,23 +6,6 @@ import sqlite3
 bd = "tEjercicio"
 from gi.repository import Gtk
 
-def userEntryChanged(self):
-    print "on_userEntry_changed"
-
-def pwdEntryChanged(self):
-    print "on_pwdEntry_changed"
-
-def emailEntryChanged():
-    print "on_emailEntry_changed"
-
-def nameEntryChanged():
-    print "on_emailEntry_changed"
-
-def surnameEntryChanged():
-    print "on_emailEntry_changed"
-
-def dirEntryChanged():
-    print "on_emailEntry_changed"
 
 def cleanBtnClicked(self):
     print "cleanBtnClicked"
@@ -48,7 +31,7 @@ def gtk_main_quit():
 if __name__ == "__main__":
     glade = Gtk.Builder()
     glade.add_from_file("Practica2_Cazalilla.glade")
-    signals = { "on_userEntry_changed": userEntryChanged,"on_pwdEntry_changed": pwdEntryChanged,"on_emailEntry_changed": emailEntryChanged,"on_nameEntry_changed": nameEntryChanged,"on_surnameEntry_changed": surnameEntryChanged,"on_dirEntry_changed": dirEntryChanged,"on_cleanBtn_clicked": cleanBtnClicked,"on_cancelBtn_clicked": cancelBtnClicked,"on_okBtn_clicked": okBtnClicked,"gtk_main_quit": Gtk.main_quit }
+    signals = { "on_cleanBtn_clicked": cleanBtnClicked,"on_cancelBtn_clicked": cancelBtnClicked,"on_okBtn_clicked": okBtnClicked,"gtk_main_quit": Gtk.main_quit }
     glade.connect_signals(signals)
     window = glade.get_object("mainWindow")
     window.show_all()
