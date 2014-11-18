c84417609018f2ec95a398a35c39b494e3955184
juankza <cazalillacostaj@gmail.com>
2014-11-18 12:58:47 +0100

    Cambios

--- Glade/Practica3_Cazalilla.py
+++ Glade/Practica3_Cazalilla.py
@@ -6,27 +6,36 @@ from gi.repository import Gtk
 
 
 ####################### FUNCTION #######################
-def entryPrecioChanged(self):
+def entryPrecioChanged():
     print "entryPrecioChanged"
 
-def entryComboBoxChanged():
-    print "entryComboBoxChanged"
+def comboBoxDescuentoChanged():
+    print "comboBoxDescuentoChanged"
 
 def gtk_main_quit():
     Gtk.main_quit()
+
+def fillComboBox():
+    discounts = 10, 20, 30, 40, 50
+    store = Gtk.ListStore(int)
+    for discount in discounts:
+        store.append([discount])
+    cellRendererText = Gtk.CellRendererText()
+    comboBoxDescuento.set_model(store)
+    comboBoxDescuento.pack_start(cellRendererText, True)
+    comboBoxDescuento.add_attribute(cellRendererText, "text", 0)
 
 
 ######################### MAIN #########################
 if __name__ == "__main__":
     glade = Gtk.Builder()
     glade.add_from_file("Practica3_Cazalilla.glade")
-    signals = { "on_entryPrecio_changed": entryPrecioChanged, "on_comboBoxEntry_changed": entryComboBoxChanged, "gtk_main_quit": Gtk.main_quit }
+    signals = { "on_entryPrecio_changed": entryPrecioChanged, "on_comboBoxDescuento_changed": comboBoxDescuentoChanged, "gtk_main_quit": Gtk.main_quit }
     glade.connect_signals(signals)
     window = glade.get_object("mainWindow")
+
+    comboBoxDescuento = glade.get_object("comboBoxDescuento")
+    fillComboBox()
+
     window.show_all()
     Gtk.main()
-
-discount = [10, 20, 30, 40, 50]
-comboBoxDescuento = glade.get_object("comboBoxDescuento")
-for i in range(3):
-    #comboBoxDescuento.append_text(discount[i])
